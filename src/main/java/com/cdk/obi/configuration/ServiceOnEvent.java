02c838074c1192323837ba02e0fd59dea3edfc21
dzs <tonyjiangtb@gmail.com>
2017-09-02 23:38:17 +0800

    add pool stop in contextclosedevent

--- src/main/java/com/cdk/obi/configuration/ServiceOnEvent.java
+++ src/main/java/com/cdk/obi/configuration/ServiceOnEvent.java
@@ -1,17 +1,31 @@
 package com.cdk.obi.configuration;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextStartedEvent;
 import org.springframework.stereotype.Component;
 
+import com.cdk.obi.service.ThreadPoolDaemon;
+
 //@Configuration
-public class ServiceOnEvent implements ApplicationListener{
-	public ServiceOnEvent(){
+public class ServiceOnEvent implements ApplicationListener {
+	public ServiceOnEvent() {
 		System.out.println("ServiceOnevent created");
 	}
+
 	public void onApplicationEvent(ApplicationEvent event) {
-		System.out.println("--------!!!!----------"+event.toString());
+		if (event instanceof ContextClosedEvent) {
+			ApplicationContext rootcontext = ((ContextClosedEvent) event).getApplicationContext();
+			//run twice here ,for servlet context can still getbean from its parent
+			ThreadPoolDaemon pool = (ThreadPoolDaemon) rootcontext.getBean("UploadPool");
+			if (pool != null) {
+				pool.Stop();
+			}
+			
+		}
+		System.out.println("--------!!!!----------" + event.toString());
 	}
 }
