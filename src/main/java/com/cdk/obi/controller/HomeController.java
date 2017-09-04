8f45c20e76918b5a05edb960fa46264d52e53440
ltg <tonyjiangtb@gmail.com>
2017-09-04 23:51:23 +0800

    responsebody return object test

--- src/main/java/com/cdk/obi/controller/HomeController.java
+++ src/main/java/com/cdk/obi/controller/HomeController.java
@@ -8,8 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cdk.obi.model.Shop;
 
 /**
  * Handles requests for the application home page.
@@ -35,5 +39,16 @@ public class HomeController {
 		
 		return "home";
 	}
+	@RequestMapping(value="/json/{name}", method = RequestMethod.GET)
+	public @ResponseBody Shop getShopInJSON(@PathVariable String name) {
+
+		Shop shop = new Shop();
+		shop.setName(name);
+		shop.setStaffName(new String[]{"mkyong1", "mkyong2"});
+
+		return shop;
+
+	}
+
 	
 }
