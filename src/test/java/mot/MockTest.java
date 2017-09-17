02e9aa4e92dfc73fdc61edbdee7f694d179865cb
tonyjiangtb <tonyjiangtb@gmail.com>
2017-09-17 23:12:55 +0800

    add mock and related POM

---
+++ src/test/java/mot/MockTest.java
@@ -0,0 +1,54 @@
+package mot;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
+public class MockTest {
+    private MockMvc mockMvc;
+
+    //@Mock
+    //private MailService mailService;
+
+    //@InjectMocks
+    //MailController mailController;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        //this.mockMvc = MockMvcBuilders.standaloneSetup(mailController).build();
+    }
+
+    @Test
+    public void should_return_status_success_when_send_mail_success() throws Exception {
+       // when(mailService.send("test@test.com", "test", "test")).thenReturn(new Result("成功"));
+
+        mockMvc.perform(post("/mail/send")
+                .param("recipients", "test@test.com")
+                .param("subject", "test")
+                .param("content", "test"))
+                .andDo(print())
+                .andExpect(status().isOk()).andExpect(content().string(is("{\"status\":\"" + "result" + "\"}")));
+
+        //verify(mailService).send("test@test.com", "test", "test");
+    }
+}
