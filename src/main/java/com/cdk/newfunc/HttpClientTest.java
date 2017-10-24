445d50a9c6d750398d785fd94699c28ca043d08c
tonyjiangtb <tonyjiangtb@gmail.com>
2017-10-24 22:28:07 +0800

    add timeout and proxy

--- src/main/java/com/cdk/newfunc/HttpClientTest.java
+++ src/main/java/com/cdk/newfunc/HttpClientTest.java
@@ -3,20 +3,31 @@ package com.cdk.newfunc;
 import java.io.IOException;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.params.CoreConnectionPNames;
 import org.apache.http.util.EntityUtils;
   
 public class HttpClientTest {  
   
 	public final static void main(String[] args) throws Exception {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+		RequestConfig requestConfig = RequestConfig.custom()  
+                .setConnectionRequestTimeout(3000) //从连接池中获取连接的超时时间     
+                .setConnectTimeout(3000) //与服务器连接超时时间：httpclient会创建一个异步线程用以创建socket连接，此处设置该socket的连接超时时间  
+                .setSocketTimeout(5000) //socket读数据超时时间：从服务器获取响应数据的超时时间  
+                .build();  
+		
+        //CloseableHttpClient httpclient = HttpClients.createDefault();
+		CloseableHttpClient httpclient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
         try {
-            HttpGet httpget = new HttpGet("https://www.baidu.com");
+            HttpGet httpget = new HttpGet("http://www.baidu.com");
 
             System.out.println("Executing request " + httpget.getRequestLine());
 
@@ -36,10 +47,16 @@ public class HttpClientTest {
                 }
 
             };
-            String responseBody = httpclient.execute(httpget, responseHandler);
+            HttpHost proxy=new HttpHost("31.135.64.76",3128,"HTTP");
+            //String responseBody = httpclient.execute(httpget, responseHandler);
+            String responseBody=httpclient.execute(proxy,httpget,responseHandler);
             System.out.println("----------------------------------------");
             System.out.println(responseBody);
-        } finally {
+        } catch (Exception e){
+        	e.printStackTrace();
+        }
+        finally {
+        
             httpclient.close();
         }
     }
